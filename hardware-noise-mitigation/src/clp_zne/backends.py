51c2a521b3bf9df1ce5262b63941bbca6c23e4c2
zaharsayapin <aasyapin2@gmail.com>
2026-02-04 19:19:25 +0300

    Expanded functionality. TwinCluterBackend was upgraded to NClusterBackend.

--- hardware-noise-mitigation/src/clp_zne/backends.py
+++ hardware-noise-mitigation/src/clp_zne/backends.py
@@ -4,11 +4,13 @@ from qiskit.transpiler import Target, InstructionProperties
 from qiskit.circuit.library import RZGate, SXGate, XGate, CZGate, Measure, IGate
 from qiskit.providers import QubitProperties
 
-class TwinClusterBackend(BackendV2):
-    def __init__(self, target, num_qubits):
-        super().__init__(name="TwinClusterBackend")
+class NClusterBackend(BackendV2):
+    def __init__(self, target, num_qubits, cluster_size, num_clusters):
+        super().__init__(name=f"{num_clusters}ClusterBackend")
         self._target = target
         self._num_qubits = num_qubits
+        self._cluster_size = cluster_size
+        self._num_clusters = num_clusters
 
     @property
     def target(self):
@@ -19,8 +21,16 @@ class TwinClusterBackend(BackendV2):
         return None
     
     @property
+    def cluster_size(self):
+        return self._cluster_size
+    
+    @property
     def num_qubits(self):
         return self._num_qubits
+    
+    @property
+    def num_clusters(self):
+        return self._num_clusters
 
     def run(self, run_input, **options):
         raise NotImplementedError("This is a synthetic backend for transpilation only.")
@@ -28,126 +38,112 @@ class TwinClusterBackend(BackendV2):
     @classmethod
     def _default_options(cls):
         return None
-    
-    @classmethod
-    def from_backend(cls, cluster_size, source_backend, c1_indices, c2_indices):
-        params = TwinClusterBackend.get_parameters_for_initialization(source_backend, c1_indices, c2_indices)
 
+    @classmethod
+    def from_backend(cls, cluster_size, source_backend, list_of_index_groups):
+        """
+        list_of_index_groups: List of lists, e.g., [[0,1,2], [10,11,12], [20,21,22]]
+        """
+        params = NClusterBackend.get_parameters_for_initialization(source_backend, list_of_index_groups)
         return cls.from_parameters(cluster_size, **params)
-    
+
     @classmethod
     def from_parameters(
-    cls, cluster_size, t1_c1, t1_c2, t2_c1, t2_c2, 
-    sq_err_c1, sq_err_c2, tq_err_c1, tq_err_c2,
-    sq_gate_time, tq_gate_time,
-    meas_time=1e-6, meas_err=0
+        cls, cluster_size, t1_stats, t2_stats, sq_err_stats, tq_err_stats,
+        sq_gate_time, tq_gate_time, meas_time=1e-6, meas_err=0
     ):
-        num_qubits = cluster_size * 2
+        """
+        t1_stats: List of (mean, std) for each cluster
+        """
+        num_clusters = len(t1_stats)
+        num_qubits = cluster_size * num_clusters
         target = Target(num_qubits=num_qubits)
         
         # --- 1. Qubit Properties ---
         qubit_props = []
-        for i in range(num_qubits):
-            # Select stats based on group
-            m_t1, s_t1 = t1_c1 if i < cluster_size else t1_c2
-            m_t2, s_t2 = t2_c1 if i < cluster_size else t2_c2
-            
-            t1 = np.random.normal(m_t1, s_t1)
-            # Physical constraint: T2 <= 2T1
-            t2 = min(np.random.normal(m_t2, s_t2), 2 * t1)
-            
-            qubit_props.append(QubitProperties(t1=t1, t2=t2))
-
-        # Assign qubit properties to target
+        for c_idx in range(num_clusters):
+            m_t1, s_t1 = t1_stats[c_idx]
+            m_t2, s_t2 = t2_stats[c_idx]
+            for _ in range(cluster_size):
+                t1 = max(0, np.random.normal(m_t1, s_t1))
+                t2 = min(np.random.normal(m_t2, s_t2), 2 * t1)
+                qubit_props.append(QubitProperties(t1=t1, t2=t2))
         target.qubit_properties = qubit_props
 
         # --- 2. Prepare Instruction Property Dictionaries ---
-        # We must collect all qubit-specific data before calling add_instruction
         x_props, sx_props, rz_props, id_props, meas_props = {}, {}, {}, {}, {}
         
-        for i in range(num_qubits):
-            m_err, s_err = sq_err_c1 if i < cluster_size else sq_err_c2
-            x_err = max(0, np.random.normal(m_err, s_err))
-            sx_err = max(0, np.random.normal(m_err, s_err))
-
-            # Define properties
-            x_phys_props = InstructionProperties(duration=sq_gate_time, error=x_err)
-            sx_phys_props = InstructionProperties(duration=sq_gate_time, error=sx_err)
-            virt_props = InstructionProperties(duration=0, error=0)
-            m_props = InstructionProperties(duration=meas_time, error=meas_err)
+        for c_idx in range(num_clusters):
+            m_err, s_err = sq_err_stats[c_idx]
+            offset = c_idx * cluster_size
             
-            # Map to qubit tuple
-            x_props[(i,)] = x_phys_props
-            sx_props[(i,)] = sx_phys_props
-            rz_props[(i,)] = virt_props
-            id_props[(i,)] = virt_props
-            meas_props[(i,)] = m_props
+            for i in range(offset, offset + cluster_size):
+                err = max(0, np.random.normal(m_err, s_err))
+                phys_p = InstructionProperties(duration=sq_gate_time, error=err)
+                virt_p = InstructionProperties(duration=0, error=0)
+                m_p = InstructionProperties(duration=meas_time, error=meas_err)
+                
+                x_props[(i,)] = phys_p
+                sx_props[(i,)] = phys_p
+                rz_props[(i,)] = virt_p
+                id_props[(i,)] = virt_p
+                meas_props[(i,)] = m_p
 
-        # Add Single Qubit Instructions to Target (called ONCE per gate type)
         target.add_instruction(XGate(), x_props)
         target.add_instruction(SXGate(), sx_props)
         target.add_instruction(RZGate(0), rz_props)
         target.add_instruction(IGate(), id_props)
         target.add_instruction(Measure(), meas_props)
 
-        # --- 3. Prepare CZ Property Dictionaries (Topology) ---
+        # --- 3. CZ Gates (Intra-cluster All-to-All) ---
         cz_props = {}
-        
-        def collect_cz(indices, stats):
-            mean_err, std_err = stats
+        for c_idx in range(num_clusters):
+            mean_err, std_err = tq_err_stats[c_idx]
+            offset = c_idx * cluster_size
+            indices = range(offset, offset + cluster_size)
+            
             for i in indices:
                 for j in indices:
                     if i != j:
                         err = max(0, np.random.normal(mean_err, std_err))
                         cz_props[(i, j)] = InstructionProperties(duration=tq_gate_time, error=err)
 
-        collect_cz(range(cluster_size), tq_err_c1)
-        collect_cz(range(cluster_size, num_qubits), tq_err_c2)
-
-        # Add Two Qubit Instruction to Target
         target.add_instruction(CZGate(), cz_props)
+        return cls(target, num_qubits, cluster_size, num_clusters)
 
-        return cls(target, num_qubits)
-    
     @staticmethod
-    def get_parameters_for_initialization(source_backend, c1_indices, c2_indices):
+    def get_parameters_for_initialization(source_backend, list_of_index_groups):
         target = source_backend.target
         
-        def extract_stats(indices):
+        all_stats = []
+        for indices in list_of_index_groups:
             t1s = [source_backend.qubit_properties(i).t1 for i in indices]
             t2s = [source_backend.qubit_properties(i).t2 for i in indices]
+            sq_e = [target['sx'][(i,)].error for i in indices if (i,) in target['sx']]
+            sq_t = [target['sx'][(i,)].duration for i in indices if (i,) in target['sx']]
             
-            # Pull errors and durations for 'sx' as the representative SQ gate
-            sq_errors = [target['sx'][(i,)].error for i in indices if (i,) in target['sx']]
-            sq_times = [target['sx'][(i,)].duration for i in indices if (i,) in target['sx']]
-
-            # Pull errors for 'cx' or 'cz' pairs strictly within this group
-            tq_errors, tq_times = [], []
+            tq_e, tq_t = [], []
             op_name = 'cz' if 'cz' in target else 'cx'
             for i in indices:
                 for j in indices:
                     if (i, j) in target[op_name]:
-                        tq_errors.append(target[op_name][(i, j)].error)
-                        tq_times.append(target[op_name][(i, j)].duration)
-            return (
-                (np.mean(t1s), np.std(t1s)),
-                (np.mean(t2s), np.std(t2s)),
-                (np.mean(sq_errors), np.std(sq_errors)),
-                (np.mean(tq_errors), np.std(tq_errors)),
-                (np.mean(sq_times), np.std(sq_times)),
-                (np.mean(tq_times), np.std(tq_times))
-            )
+                        tq_e.append(target[op_name][(i, j)].error)
+                        tq_t.append(target[op_name][(i, j)].duration)
+            
+            all_stats.append({
+                "t1": (np.mean(t1s), np.std(t1s)),
+                "t2": (np.mean(t2s), np.std(t2s)),
+                "sq_e": (np.mean(sq_e), np.std(sq_e)),
+                "tq_e": (np.mean(tq_e), np.std(tq_e)),
+                "sq_t": np.mean(sq_t),
+                "tq_t": np.mean(tq_t)
+            })
 
-        stats1 = extract_stats(c1_indices)
-        stats2 = extract_stats(c2_indices)
-        
         return {
-            "t1_c1": stats1[0], "t2_c1": stats1[1], "sq_err_c1": stats1[2], "tq_err_c1": stats1[3],
-            "t1_c2": stats2[0], "t2_c2": stats2[1], "sq_err_c2": stats2[2], "tq_err_c2": stats2[3],
-            "sq_gate_time": (stats1[4][0]+stats2[4][0])/2, 
-            "tq_gate_time": (stats1[5][0]+stats2[5][0])/2,
+            "t1_stats": [s["t1"] for s in all_stats],
+            "t2_stats": [s["t2"] for s in all_stats],
+            "sq_err_stats": [s["sq_e"] for s in all_stats],
+            "tq_err_stats": [s["tq_e"] for s in all_stats],
+            "sq_gate_time": np.mean([s["sq_t"] for s in all_stats]),
+            "tq_gate_time": np.mean([s["tq_t"] for s in all_stats]),
         }
-    
-    def run(self, run_input, **options):
-        raise NotImplementedError("This is a synthetic backend for transpilation only.")
-        
